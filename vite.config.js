588e4f104f0ee5c8bb06cbd78a0f51f1b0ae4585
voltDeveloper <vladimir305200@mail.ru>
2024-06-16 15:08:35 +0500

    install libs part 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 3,
+          },
+        },
+      },
+    }),
+  ],
+  define: {
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@img": path.resolve(__dirname, "./public/img"),
+    },
+  },
+});
